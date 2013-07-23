f72c3c59939c154c67262640d39c6fd1c8d7aedd
Bruno <bruno.casella@mstech.com.br>
2013-07-23 10:59:39 -0300

    Mudado para usar o BUILD_ID. E versao mudada para 2.0.0

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
 import os
 from setuptools import setup
-from carbono.config import get_version
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+VERSION_ID = "2.0.0"
+BUILD_ID = "{0}-SNAPSHOT".format(VERSION_ID)
+if 'BUILD_NUMBER' in os.environ:
+    BUILD_ID = "{0}.{1}".format(VERSION_ID, os.environ['BUILD_NUMBER'])
 
 setup(
     name = "carbono",
-    version = get_version(),
+    version = BUILD_ID,
     author = "Bruno Casella",
     author_email = "bruno.casella@gmail.com",
     description = ("A hard disk imaging and recovery application"),
